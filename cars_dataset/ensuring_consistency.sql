74ecefe5575d274f88db0ae36398981884c3d4a7
Daniel Gonçalves Dutra <daniel.dutra.45@gmail.com>
2022-09-03 22:01:45 -0300

    changes to cars_dataset

--- cars_dataset/ensuring_consistency.sql
+++ cars_dataset/ensuring_consistency.sql
@@ -1,15 +1,15 @@
-/*Finally, you want to check your data for any inconsistencies that might cause errors. These inconsistencies can be tricky
-to spot — sometimes even something as simple as an extra space can cause a problem.
-Check the drive_wheels column for inconsistencies by running a query with a SELECT DISTINCT statement:*/
+/*Finally, you want to check your data for any inconsistencies that might cause errors. These inconsistencies can 
+be trickyto spot — sometimes even something as simple as an extra space can cause a problem. Check the drive_wheels
+column for inconsistencies by running a query with a SELECT DISTINCT statement:*/
 
 SELECT
     DISTINCT drive_wheels
 FROM
     cars.car_info;
 
-/*It appears that 4wd appears twice in results. However, because you used a SELECT DISTINCT statement to return unique values,
-this probably means there’s an extra space in one of the 4wd entries that makes it different from the other 4wd.
-To check if this is the case, you can use a LENGTH statement to determine the length of how long each of these string variables:*/
+/*It appears that 4wd appears twice in results. However, because you used a SELECT DISTINCT statement to return unique 
+values, this probably means there’s an extra space in one of the 4wd entries that makes it different from the other 4wd. To check
+if this is the case, you can use a LENGTH statement to determine the length of how long each of these string variables:*/
 
 SELECT
     DISTINCT drive_wheels,
@@ -17,8 +17,8 @@ SELECT
 FROM
     cars.car_info;
 
-/*According to these results, some instances of the 4wd string have four characters instead of the expected three (4wd has 3 characters).
-In that case, you can use the TRIM function to remove all extra spaces in the drive_wheels column: */
+/*According to these results, some instances of the 4wd string have four characters instead of the expected three
+(4wd has 3 characters). In that case, you can use the TRIM function to remove all extra spaces in the drive_wheels column:*/
 
 UPDATE
     cars.car_info
@@ -26,11 +26,11 @@ SET
     drive_wheels = TRIM(drive_wheels)
 WHERE TRUE;
 
-/*Then, you run the SELECT DISTINCT statement again to ensure that there are only three distinct values in the drive_wheels column: */
+/*Then, you run the SELECT DISTINCT statement again to ensure that there are only three distinct values in the drive_wheels column:*/
 
 SELECT
     DISTINCT drive_wheels
 FROM
     cars.car_info;
 
-/*And now there should only be three unique values in this column! Which means your data is clean, consistent, and ready for analysis! */
+/*And now there should only be three unique values in this column! Which means your data is clean, consistent, and ready for analysis!*/
--- cars_dataset/identifying_potential_errors.sql
+++ cars_dataset/identifying_potential_errors.sql
@@ -1,4 +1,4 @@
-/*Checking for any misspelling errors in the num_of_cylinders column*/
+/*Checking for any misspelling errors in the num_of_cylinders column.*/
 SELECT
     DISTINCT num_of_cylinders
 FROM
@@ -12,15 +12,15 @@ SET
 WHERE
     num_of_cylinders = "tow";
 
-/*Cheking if the compression_ratio column values range from 7 to 23*/
+/*Cheking if the compression_ratio column values range from 7 to 23.*/
 SELECT
     MIN(compression_ratio) AS min_compression_ratio,
     MAX(compression_ratio) AS max_compression_ratio
 FROM
     cars.car_info;
 
-/*This returns a maximum of 70. But we know this is an error because the maximum value in this 
-column should be 23, not 70. So the 70 is most likely a 7.0. Run the above query again without 
+/*This returns a maximum of 70. But we know this is an error because the maximum value in this
+column should be 23, not 70. So the 70 is most likely a 7.0. Run the above query again without
 the row with 70 to make sure that the rest of the values fall within the expected range of 7 to 23.*/
 SELECT
     MIN(compression_ratio) AS min_compression_ratio,
@@ -30,12 +30,12 @@ FROM
 WHERE
     compression_ratio <> 70;
 
-/*Now the highest value is 23, which aligns with the data description. So you’ll want to correct the 70 value. 
-You check with the sales manager again, who says that this row was made in error and should be removed. Before 
-you delete anything, you should check to see how many rows contain this erroneous value as a precaution so that 
-you don’t end up deleting 50% of your data. If there are too many (for instance, 20% of your rows have the incorrect 70 value),
- then you would want to check back in with the sales manager to inquire if these should be deleted or if the 70 should be updated
-  to another value. Use the query below to count how many rows you would be deleting:*/
+/*Now the highest value is 23, which aligns with the data description. So you’ll want to correct the 70 value.
+You check with the sales manager again, who says that this row was made in error and should be removed. Before
+you delete anything, you should check to see how many rows contain this erroneous value as a precaution so that
+you don’t end up deleting 50% of your data. If there are too many (for instance, 20% of your rows have the incorrect 70 value)
+then you would want to check back in with the sales manager to inquire if these should be deleted or if the 70 should be updated
+to another value. Use the query below to count how many rows you would be deleting:*/
 
 SELECT
 
@@ -49,7 +49,7 @@ WHERE
 
     compression_ratio = 70;
 
-/*Turns out there is only one row with the erroneous 70 value. So you can delete that row using this query: */
+/*Turns out there is only one row with the erroneous 70 value. So you can delete that row using this query:*/
 
 DELETE cars.car_info
 
