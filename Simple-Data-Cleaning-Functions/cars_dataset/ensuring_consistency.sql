d58d40e5dee1614f821140d6c409a712b9642f38
Daniel Gonçalves Dutra <daniel.dutra.45@gmail.com>
2022-09-04 00:29:28 -0300

    Update ensuring_consistency.sql

--- Simple-Data-Cleaning-Functions/cars_dataset/ensuring_consistency.sql
+++ Simple-Data-Cleaning-Functions/cars_dataset/ensuring_consistency.sql
@@ -1,7 +1,6 @@
 /*Finally, you want to check your data for any inconsistencies that might cause errors. These inconsistencies can 
 be trickyto spot — sometimes even something as simple as an extra space can cause a problem. Check the drive_wheels
 column for inconsistencies by running a query with a SELECT DISTINCT statement:*/
-
 SELECT
     DISTINCT drive_wheels
 FROM
@@ -10,7 +9,6 @@ FROM
 /*It appears that 4wd appears twice in results. However, because you used a SELECT DISTINCT statement to return unique 
 values, this probably means there’s an extra space in one of the 4wd entries that makes it different from the other 4wd. To check
 if this is the case, you can use a LENGTH statement to determine the length of how long each of these string variables:*/
-
 SELECT
     DISTINCT drive_wheels,
     LENGTH(drive_wheels) AS string_length
@@ -19,7 +17,6 @@ FROM
 
 /*According to these results, some instances of the 4wd string have four characters instead of the expected three
 (4wd has 3 characters). In that case, you can use the TRIM function to remove all extra spaces in the drive_wheels column:*/
-
 UPDATE
     cars.car_info
 SET
@@ -27,7 +24,6 @@ SET
 WHERE TRUE;
 
 /*Then, you run the SELECT DISTINCT statement again to ensure that there are only three distinct values in the drive_wheels column:*/
-
 SELECT
     DISTINCT drive_wheels
 FROM
